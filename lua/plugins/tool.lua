0161d2e70b0fd83dc2e1c740580ee312a4a19e37
r1v3r <churiver@outlook.com>
2023-06-07 00:09:59 +0800

    feat: complete tool plugins

--- lua/plugins/tool.lua
+++ lua/plugins/tool.lua
@@ -13,15 +13,15 @@ tool["nvim-tree/nvim-tree.lua"] = {
     cmd = {"NvimTreeToggle", "NvimTreeOpen", "NvimTreeFindFile", "NvimTreeFindFileToggle", "NvimTreeRefresh"},
     config = require("tool.nvim-tree")
 }
+tool["tpope/vim-fugitive"] = {
+    lazy = true,
+    cmd = {"Git", "G"}
+}
 tool["gelguy/wilder.nvim"] = {
     lazy = true,
     event = "CmdlineEnter",
     config = require("tool.wilder"),
     dependencies = {"romgrk/fzy-lua-native"}
-}
-tool["tpope/vim-fugitive"] = {
-    lazy = true,
-    cmd = {"Git", "G"}
 }
 tool["kdheepak/lazygit.nvim"] = {
     lazy = true,
@@ -29,6 +29,69 @@ tool["kdheepak/lazygit.nvim"] = {
     config = require("tool.lazygit"),
     -- optional for floating window border decoration
     dependencies = {"nvim-lua/plenary.nvim"}
+}
+tool["ibhagwan/smartyank.nvim"] = {
+    lazy = true,
+    event = "BufReadPost",
+    config = require("tool.smartyank")
+}
+tool["michaelb/sniprun"] = {
+    lazy = true,
+    -- You need to cd to `~/.local/share/nvim/site/lazy/sniprun/` and execute `bash ./install.sh`,
+    -- if you encountered error about no executable sniprun found.
+    build = "bash ./install.sh",
+    cmd = {"SnipRun"},
+    config = require("tool.sniprun")
+}
+tool["akinsho/toggleterm.nvim"] = {
+    lazy = true,
+    cmd = {"ToggleTerm", "ToggleTermSetName", "ToggleTermToggleAll", "ToggleTermSendVisualLines",
+           "ToggleTermSendCurrentLine", "ToggleTermSendVisualSelection"},
+    config = require("tool.toggleterm")
+}
+tool["folke/trouble.nvim"] = {
+    lazy = true,
+    cmd = {"Trouble", "TroubleToggle", "TroubleRefresh"},
+    config = require("tool.trouble")
+}
+tool["folke/which-key.nvim"] = {
+    lazy = true,
+    event = {"CursorHold", "CursorHoldI"},
+    config = require("tool.which-key")
+}
+
+----------------------------------------------------------------------
+--                        Telescope Plugins                         --
+----------------------------------------------------------------------
+tool["nvim-telescope/telescope.nvim"] = {
+    lazy = true,
+    cmd = "Telescope",
+    config = require("tool.telescope"),
+    dependencies = {{"nvim-tree/nvim-web-devicons"}, {"nvim-lua/plenary.nvim"}, {"debugloop/telescope-undo.nvim"}, {
+        "ahmedkhalf/project.nvim",
+        event = "BufReadPost",
+        config = require("tool.project")
+    }, {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make"
+    }, {
+        "nvim-telescope/telescope-frecency.nvim",
+        dependencies = {{"kkharji/sqlite.lua"}}
+    }, {"jvgrootveld/telescope-zoxide"}, {"nvim-telescope/telescope-live-grep-args.nvim"}}
+}
+
+----------------------------------------------------------------------
+--                           DAP Plugins                            --
+----------------------------------------------------------------------
+tool["mfussenegger/nvim-dap"] = {
+    lazy = true,
+    cmd = {"DapSetLogLevel", "DapShowLog", "DapContinue", "DapToggleBreakpoint", "DapToggleRepl", "DapStepOver",
+           "DapStepInto", "DapStepOut", "DapTerminate"},
+    config = require("tool.dap"),
+    dependencies = {{
+        "rcarriga/nvim-dap-ui",
+        config = require("tool.dap.dapui")
+    }}
 }
 
 -- 	-- Unmanaged plugin (manually installed and updated)
--- lua/r1v3r/keymaps.lua
+++ lua/r1v3r/keymaps.lua
@@ -182,11 +182,199 @@ vim.keymap.set("", "<LEADER>sw", ":set wrap<CR>", nsopts)
 
 -- ==================== Plugins Keymaps ====================
 local setPluginKeys = function()
-    -- nvim tree
+    -- Plugin: vim-fugitive
+    -- ["n|gps"] = map_cr("G push"):with_noremap():with_silent():with_desc("git: Push"),
+    -- ["n|gpl"] = map_cr("G pull"):with_noremap():with_silent():with_desc("git: Pull"),
+    -- ["n|<leader>G"] = map_cu("Git"):with_noremap():with_silent():with_desc("git: Open git-fugitive"),
+
+    -- Plugin: nvim-tree
     vim.keymap.set("", "tt", ":NvimTreeToggle<CR>", nsopts)
     vim.keymap.set("", "tf", ":NvimTreeFocus<CR>", nsopts)
-    -- lazygit
+
+    --  Plugin:lazygit
     vim.keymap.set("", "<C-g>", ":LazyGit<CR>", nsopts)
+
+    -- Plugin: sniprun
+    vim.keymap.set("v", "<LEADER>r", ":SnipRun<CR>", nsopts) -- tool: Run code by range
+    vim.keymap.set("n", "<LEADER>r", ":SnipRun<CR>", nsopts) -- tool: Run code by file
+
+    -- -- Plugin: toggleterm
+	-- ["t|<Esc><Esc>"] = map_cmd([[<C-\><C-n>]]):with_noremap():with_silent(), -- switch to normal mode in terminal.
+	-- ["t|jk"] = map_cmd([[<C-\><C-n>]]):with_noremap():with_silent(), -- switch to normal mode in terminal.
+	-- ["n|<C-\\>"] = map_cr([[execute v:count . "ToggleTerm direction=horizontal"]])
+	-- 	:with_noremap()
+	-- 	:with_silent()
+	-- 	:with_desc("terminal: Toggle horizontal"),
+	-- ["i|<C-\\>"] = map_cmd("<Esc><Cmd>ToggleTerm direction=horizontal<CR>")
+	-- 	:with_noremap()
+	-- 	:with_silent()
+	-- 	:with_desc("terminal: Toggle horizontal"),
+	-- ["t|<C-\\>"] = map_cmd("<Cmd>ToggleTerm<CR>"):with_noremap():with_silent():with_desc("terminal: Toggle horizontal"),
+	-- ["n|<A-\\>"] = map_cr([[execute v:count . "ToggleTerm direction=vertical"]])
+	-- 	:with_noremap()
+	-- 	:with_silent()
+	-- 	:with_desc("terminal: Toggle vertical"),
+	-- ["i|<A-\\>"] = map_cmd("<Esc><Cmd>ToggleTerm direction=vertical<CR>")
+	-- 	:with_noremap()
+	-- 	:with_silent()
+	-- 	:with_desc("terminal: Toggle vertical"),
+	-- ["t|<A-\\>"] = map_cmd("<Cmd>ToggleTerm<CR>"):with_noremap():with_silent():with_desc("terminal: Toggle vertical"),
+	-- ["n|<F5>"] = map_cr([[execute v:count . "ToggleTerm direction=vertical"]])
+	-- 	:with_noremap()
+	-- 	:with_silent()
+	-- 	:with_desc("terminal: Toggle vertical"),
+	-- ["i|<F5>"] = map_cmd("<Esc><Cmd>ToggleTerm direction=vertical<CR>")
+	-- 	:with_noremap()
+	-- 	:with_silent()
+	-- 	:with_desc("terminal: Toggle vertical"),
+	-- ["t|<F5>"] = map_cmd("<Cmd>ToggleTerm<CR>"):with_noremap():with_silent():with_desc("terminal: Toggle vertical"),
+	-- ["n|<A-d>"] = map_cr([[execute v:count . "ToggleTerm direction=float"]])
+	-- 	:with_noremap()
+	-- 	:with_silent()
+	-- 	:with_desc("terminal: Toggle float"),
+	-- ["i|<A-d>"] = map_cmd("<Esc><Cmd>ToggleTerm direction=float<CR>")
+	-- 	:with_noremap()
+	-- 	:with_silent()
+	-- 	:with_desc("terminal: Toggle float"),
+	-- ["t|<A-d>"] = map_cmd("<Cmd>ToggleTerm<CR>"):with_noremap():with_silent():with_desc("terminal: Toggle float"),
+	-- ["n|<leader>g"] = map_callback(function()
+	-- 		_toggle_lazygit()
+	-- 	end)
+	-- 	:with_noremap()
+	-- 	:with_silent()
+	-- 	:with_desc("git: Toggle lazygit"),
+
+	-- -- Plugin: trouble
+	-- ["n|gt"] = map_cr("TroubleToggle"):with_noremap():with_silent():with_desc("lsp: Toggle trouble list"),
+	-- ["n|<leader>tr"] = map_cr("TroubleToggle lsp_references")
+	-- 	:with_noremap()
+	-- 	:with_silent()
+	-- 	:with_desc("lsp: Show lsp references"),
+	-- ["n|<leader>td"] = map_cr("TroubleToggle document_diagnostics")
+	-- 	:with_noremap()
+	-- 	:with_silent()
+	-- 	:with_desc("lsp: Show document diagnostics"),
+	-- ["n|<leader>tw"] = map_cr("TroubleToggle workspace_diagnostics")
+	-- 	:with_noremap()
+	-- 	:with_silent()
+	-- 	:with_desc("lsp: Show workspace diagnostics"),
+	-- ["n|<leader>tq"] = map_cr("TroubleToggle quickfix")
+	-- 	:with_noremap()
+	-- 	:with_silent()
+	-- 	:with_desc("lsp: Show quickfix list"),
+	-- ["n|<leader>tl"] = map_cr("TroubleToggle loclist"):with_noremap():with_silent():with_desc("lsp: Show loclist"),
+
+	-- -- Plugin: telescope
+	-- ["n|<C-p>"] = map_callback(function()
+	-- 		_command_panel()
+	-- 	end)
+	-- 	:with_noremap()
+	-- 	:with_silent()
+	-- 	:with_desc("tool: Toggle command panel"),
+	-- ["n|<leader>u"] = map_callback(function()
+	-- 		require("telescope").extensions.undo.undo()
+	-- 	end)
+	-- 	:with_noremap()
+	-- 	:with_silent()
+	-- 	:with_desc("edit: Show undo history"),
+	-- ["n|<leader>fp"] = map_callback(function()
+	-- 		require("telescope").extensions.projects.projects({})
+	-- 	end)
+	-- 	:with_noremap()
+	-- 	:with_silent()
+	-- 	:with_desc("find: Project"),
+	-- ["n|<leader>fr"] = map_callback(function()
+	-- 		require("telescope").extensions.frecency.frecency()
+	-- 	end)
+	-- 	:with_noremap()
+	-- 	:with_silent()
+	-- 	:with_desc("find: File by frecency"),
+	-- ["n|<leader>fw"] = map_callback(function()
+	-- 		require("telescope").extensions.live_grep_args.live_grep_args()
+	-- 	end)
+	-- 	:with_noremap()
+	-- 	:with_silent()
+	-- 	:with_desc("find: Word in project"),
+	-- ["n|<leader>fe"] = map_cu("Telescope oldfiles"):with_noremap():with_silent():with_desc("find: File by history"),
+	-- ["n|<leader>ff"] = map_cu("Telescope find_files"):with_noremap():with_silent():with_desc("find: File in project"),
+	-- ["n|<leader>fc"] = map_cu("Telescope colorscheme")
+	-- 	:with_noremap()
+	-- 	:with_silent()
+	-- 	:with_desc("ui: Change colorscheme for current session"),
+	-- ["n|<leader>fn"] = map_cu(":enew"):with_noremap():with_silent():with_desc("buffer: New"),
+	-- ["n|<leader>fg"] = map_cu("Telescope git_files")
+	-- 	:with_noremap()
+	-- 	:with_silent()
+	-- 	:with_desc("find: file in git project"),
+	-- ["n|<leader>fz"] = map_cu("Telescope zoxide list")
+	-- 	:with_noremap()
+	-- 	:with_silent()
+	-- 	:with_desc("edit: Change current direrctory by zoxide"),
+	-- ["n|<leader>fb"] = map_cu("Telescope buffers"):with_noremap():with_silent():with_desc("find: Buffer opened"),
+	-- ["n|<leader>fs"] = map_cu("Telescope grep_string"):with_noremap():with_silent():with_desc("find: Current word"),
+
+	-- -- Plugin: dap
+	-- ["n|<F6>"] = map_callback(function()
+	-- 		require("dap").continue()
+	-- 	end)
+	-- 	:with_noremap()
+	-- 	:with_silent()
+	-- 	:with_desc("debug: Run/Continue"),
+	-- ["n|<F7>"] = map_callback(function()
+	-- 		require("dap").terminate()
+	-- 		require("dapui").close()
+	-- 	end)
+	-- 	:with_noremap()
+	-- 	:with_silent()
+	-- 	:with_desc("debug: Stop"),
+	-- ["n|<F8>"] = map_callback(function()
+	-- 		require("dap").toggle_breakpoint()
+	-- 	end)
+	-- 	:with_noremap()
+	-- 	:with_silent()
+	-- 	:with_desc("debug: Toggle breakpoint"),
+	-- ["n|<F9>"] = map_callback(function()
+	-- 		require("dap").step_into()
+	-- 	end)
+	-- 	:with_noremap()
+	-- 	:with_silent()
+	-- 	:with_desc("debug: Step into"),
+	-- ["n|<F10>"] = map_callback(function()
+	-- 		require("dap").step_out()
+	-- 	end)
+	-- 	:with_noremap()
+	-- 	:with_silent()
+	-- 	:with_desc("debug: Step out"),
+	-- ["n|<F11>"] = map_callback(function()
+	-- 		require("dap").step_over()
+	-- 	end)
+	-- 	:with_noremap()
+	-- 	:with_silent()
+	-- 	:with_desc("debug: Step over"),
+	-- ["n|<leader>db"] = map_callback(function()
+	-- 		require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
+	-- 	end)
+	-- 	:with_noremap()
+	-- 	:with_silent()
+	-- 	:with_desc("debug: Set breakpoint with condition"),
+	-- ["n|<leader>dc"] = map_callback(function()
+	-- 		require("dap").run_to_cursor()
+	-- 	end)
+	-- 	:with_noremap()
+	-- 	:with_silent()
+	-- 	:with_desc("debug: Run to cursor"),
+	-- ["n|<leader>dl"] = map_callback(function()
+	-- 		require("dap").run_last()
+	-- 	end)
+	-- 	:with_noremap()
+	-- 	:with_silent()
+	-- 	:with_desc("debug: Run last"),
+	-- ["n|<leader>do"] = map_callback(function()
+	-- 		require("dap").repl.open()
+	-- 	end)
+	-- 	:with_noremap()
+	-- 	:with_silent()
+	-- 	:with_desc("debug: Open REPL"),
 end
 
 setPluginKeys()
