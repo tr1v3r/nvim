a3f860e8d0a0d30d05907929b32be89cfe999034
r1v3r <churiver@outlook.com>
2023-06-09 21:32:35 +0800

    feat: optimize options and event

--- lua/r1v3r/options.lua
+++ lua/r1v3r/options.lua
@@ -1,81 +1,115 @@
--- ================ Settings ================
-vim.o.termguicolors = true
-vim.o.t_Co = 256 -- vim-monokai now only support 256 colours in terminal.
+-- ================ Options ================
+local o = vim.o
+local wo = vim.wo
+local global = require("r1v3r.global")
 
--- Line numbers
-vim.wo.number = true
-vim.wo.relativenumber = true
--- Show command in status line
-vim.o.showcmd = true
--- Enable wildmenu
-vim.o.wildmenu = true
--- Enable reading configuration from current directory
-vim.o.exrc = true
--- Enable secure mode
-vim.o.secure = true
--- Use spaces instead of tabs
-vim.o.expandtab = true
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
-vim.o.softtabstop = 4
--- Enable autoindent
-vim.o.autoindent = true
--- Disable indent expression
-vim.o.indentexpr = ''
--- Show whitespace characters
-vim.wo.list = true
-vim.o.listchars = 'tab:| ,trail:☒'
--- Set the number of lines to keep above and below the cursor in scroll off
-vim.o.scrolloff = 4
--- Disable the time delay for key codes
-vim.o.ttimeoutlen = 0
-vim.o.timeout = false
--- Set view options for saving and restoring folds, slash search, Unix line endings
-vim.o.viewoptions = 'cursor,folds,slash,unix'
--- Highlight current line
-vim.wo.cursorline = true
--- Enable line wrapping
-vim.wo.wrap = true
--- Disable text width
-vim.o.tw = 0
--- Set fold method to syntax
-vim.o.foldmethod = 'syntax'
--- Set fold level to maximum
-vim.o.foldlevel = 99
--- Enable folding
-vim.o.foldenable = true
--- Disable automatic formatting for comments
-vim.o.formatoptions = vim.o.formatoptions:gsub('tc', '')
--- Split windows to the right and below
-vim.o.splitright = true
-vim.o.splitbelow = true
--- Hide mode from status line
-vim.o.showmode = false
--- Enable search highlighting
-vim.o.hlsearch = true
--- Enable incremental search
-vim.o.incsearch = true
--- Ignore case in search unless an uppercase character is used
-vim.o.ignorecase = true
-vim.o.smartcase = true
--- Shorten error messages
-vim.o.shortmess = vim.o.shortmess .. 'flnrwx'
--- Open command-line window in split mode
-vim.o.inccommand = 'split'
--- Set completion options
-vim.o.completeopt = 'noinsert,longest,menuone,noselect,preview'
--- Enable lazy redraw
-vim.o.lazyredraw = true
--- Use visual bell instead of beeping sound
-vim.o.visualbell = true
--- Highlight a column at a specific width
-vim.wo.colorcolumn = '120'
--- Set the time interval for writing swap and backup files
-vim.o.updatetime = 100
--- Enable blockwise virtual editing
-vim.o.virtualedit = 'block'
--- Automatically change the current directory to the directory of the current file
-vim.o.autochdir = true
--- Automatically write all buffers when executing certain commands
-vim.o.autowriteall = true
--- vim.o.signcolumn = 'nubmer'
+-- Syntax highlighting
+vim.cmd('syntax on')
+-- Disable search highlighting after executing a search
+vim.cmd([[exec "nohlsearch"]])
+
+local options = {}
+
+function options.o()
+    return {
+        termguicolors = true,
+        t_Co = 256, -- vim-monokai now only support 256 colours in terminal.
+
+        -- Show command in status line
+        showcmd = true,
+        -- Enable wildmenu
+        wildmenu = true,
+        -- Enable reading configuration from current directory
+        exrc = true,
+        -- Enable secure mode
+        secure = true,
+        -- Use spaces instead of tabs
+        expandtab = true,
+        tabstop = 4,
+        shiftwidth = 4,
+        softtabstop = 4,
+
+        -- Enable autoindent
+        autoindent = true,
+        -- Disable indent expression
+        indentexpr = '',
+        -- Show whitespace characters
+        listchars = 'tab:| ,trail:☒',
+        -- Set the number of lines to keep above and below the cursor in scroll off
+        scrolloff = 4,
+        -- Disable the time delay for key codes
+        ttimeoutlen = 0,
+        timeout = false,
+        -- Set view options for saving and restoring folds, slash search, Unix line endings
+        viewoptions = 'cursor,folds,slash,unix',
+        -- Disable text width
+        tw = 0,
+        -- Set fold method to syntax
+        foldmethod = 'syntax',
+        -- Set fold level to maximum
+        foldlevel = 99,
+        -- Enable folding
+        foldenable = true,
+        -- Disable automatic formatting for comments
+        formatoptions = o.formatoptions:gsub('tc', ''),
+        -- Split windows to the right and below
+        splitright = true,
+        splitbelow = true,
+        -- Hide mode from status line
+        showmode = false,
+        -- Enable search highlighting
+        hlsearch = true,
+        -- Enable incremental search
+        incsearch = true,
+        -- Ignore case in search unless an uppercase character is used
+        ignorecase = true,
+        smartcase = true,
+        -- Shorten error messages
+        shortmess = o.shortmess .. 'flnrwx',
+        -- Open command-line window in split mode
+        inccommand = 'split',
+        -- Set completion options
+        completeopt = 'noinsert,longest,menuone,noselect,preview',
+        -- Enable lazy redraw
+        lazyredraw = true,
+        -- Use visual bell instead of beeping sound
+        visualbell = true,
+        -- Set the time interval for writing swap and backup files
+        updatetime = 100,
+        -- Enable blockwise virtual editing
+        virtualedit = 'block',
+        -- Automatically change the current directory to the directory of the current file
+        autochdir = true,
+        -- Automatically write all buffers when executing certain commands
+        autowriteall = true,
+        -- signcolumn = 'nubmer'
+        undodir = global.cacheDir .. "undo/"
+    }
+end
+
+function options.wo()
+    return {
+        list = true,
+        -- Line numbers
+        number = true,
+        relativenumber = true,
+
+        -- Highlight current line
+        cursorline = true,
+        -- Enable line wrapping
+        wrap = true,
+        -- Highlight a column at a specific width
+        colorcolumn = '120'
+    }
+end
+
+function options.init()
+    for k, v in pairs(options.o()) do
+        o[k] = v
+    end
+    for k, v in pairs(options.wo()) do
+        wo[k] = v
+    end
+end
+
+options.init()
