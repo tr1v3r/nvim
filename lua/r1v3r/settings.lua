d2e03335f03dadb0b3652dea66c93a6f5885bd25
r1v3r <churiver@outlook.com>
2023-06-13 21:16:57 +0800

    feat: update lsp deps

--- lua/r1v3r/settings.lua
+++ lua/r1v3r/settings.lua
@@ -76,14 +76,13 @@ settings["load_big_files_faster"] = true
 -- check the below link for all the supported LSPs:
 -- https://github.com/neovim/nvim-lspconfig/tree/master/lua/lspconfig/server_configurations
 ---@type string[]
-settings["lsp_deps"] = {"bashls", "clangd", "html", "jsonls", "lua_ls", "pylsp" -- "gopls",
-}
+settings["lsp_deps"] = {"bashls", "clangd", "html", "jsonls", "lua_ls", "pylsp", "gopls"}
 
 -- Set the general-purpose servers that will be installed during bootstrap here.
 -- Check the below link for all supported sources.
 -- in `code_actions`, `completion`, `diagnostics`, `formatting`, `hover` folders:
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins
 ---@type string[]
-settings["null_ls_deps"] = {"clang_format", "prettier", "rustfmt", "shfmt", "stylua", "vint"}
+settings["null_ls_deps"] = {"clang_format", "prettier", "rustfmt", "shfmt", "stylua", "vint", "goimports"}
 
 return settings
