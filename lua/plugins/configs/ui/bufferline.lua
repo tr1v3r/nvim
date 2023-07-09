4f1eab651de28de9ffdf4ba4a57c62d07674c792
r1v3r <churiver@outlook.com>
2023-07-09 19:40:58 +0800

    feat(ui): remove buffer close icon

--- lua/plugins/configs/ui/bufferline.lua
+++ lua/plugins/configs/ui/bufferline.lua
@@ -13,13 +13,13 @@ return function()
 			tab_size = 20,
 			color_icons = true,
 			show_buffer_icons = true,
-			show_buffer_close_icons = true,
+			show_buffer_close_icons = false,
 			show_close_icon = true,
 			show_tab_indicators = true,
 			enforce_regular_tabs = true,
 			persist_buffer_sort = true,
 			always_show_bufferline = true,
-			separator_style = "thin",
+			separator_style = "thin", -- slant/padded_slant/slope/padded_slope/thick/thin
 			diagnostics = "nvim_lsp",
 			diagnostics_indicator = function(count)
 				return "(" .. count .. ")"
