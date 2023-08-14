859c852f36f303f721d4d2a7bc236724c9eea7dd
r1v3r <churiver@outlook.com>
2023-08-14 18:49:24 +0800

    feat(dap): update dap configs

--- lua/plugins/configs/tool/dap/dap-keymaps.lua
+++ lua/plugins/configs/tool/dap/dap-keymaps.lua
@@ -6,7 +6,7 @@ local did_load_debug_mappings = false
 
 function M.load_extras()
 	if not did_load_debug_mappings then
-		map("K", "<Cmd>lua require('dapui').eval()<CR>"):mode("nv"):noremap():nowait():desc("dap: Evaluate expression under cursor"):set()
+		map("W", "lua require('dapui').eval()"):mode("nv"):Cmd():noremap():nowait():desc("dap: Evaluate expression under cursor"):set()
 		did_load_debug_mappings = true
 	end
 end
