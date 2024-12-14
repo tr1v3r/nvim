87774fe57ac139a771be3d4ee461183234b29226
tr1v3r <churiver@outlook.com>
2024-12-15 00:42:11 +0800

    feat: set python host prog to CONDA_PYTHON_EXE

--- lua/r1v3r/options.lua
+++ lua/r1v3r/options.lua
@@ -134,19 +134,19 @@ function options.wo()
 	}
 end
 
-local function isempty(s)
-	return s == nil or s == ""
-end
+-- local function isempty(s)
+-- 	return s == nil or s == ""
+-- end
 local function use_if_defined(val, fallback)
 	return val ~= nil and val or fallback
 end
 
 function options.set_provider()
 	-- custom python provider
-	local conda_prefix = os.getenv("CONDA_PREFIX")
-	if not isempty(conda_prefix) then
-		vim.g.python_host_prog = use_if_defined(vim.g.python_host_prog, conda_prefix .. "/bin/python")
-		vim.g.python3_host_prog = use_if_defined(vim.g.python3_host_prog, conda_prefix .. "/bin/python")
+	local conda_python_exe = os.getenv("CONDA_PYTHON_EXE")
+	if conda_python_exe then
+		vim.g.python_host_prog = use_if_defined(vim.g.python_host_prog, conda_python_exe)
+		vim.g.python3_host_prog = use_if_defined(vim.g.python3_host_prog, conda_python_exe)
 	else
 		vim.g.python_host_prog = use_if_defined(vim.g.python_host_prog, "python")
 		vim.g.python3_host_prog = use_if_defined(vim.g.python3_host_prog, "python3")
