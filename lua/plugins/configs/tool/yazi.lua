988ad27b797aa4305da2766c2e4b3360b6d9beee
tr1v3r <churiver@outlook.com>
2025-02-21 14:33:42 +0800

    fix(plugins): fix yazi.nvim config error

--- lua/plugins/configs/tool/yazi.lua
+++ lua/plugins/configs/tool/yazi.lua
@@ -1,21 +1,23 @@
 return function()
 	require("yazi").setup({
-		opts = {
-			floating_window_scaling_factor = 1,
-			yazi_floating_window_border = "none",
-			open_for_directories = true,
-			open_multiple_tabs = true,
-			keymaps = {
-				show_help = "<f1>",
-				open_file_in_vertical_split = "<c-v>",
-				open_file_in_horizontal_split = "<c-x>",
-				open_file_in_tab = "<c-t>",
-				grep_in_directory = "<c-f>",
-				replace_in_directory = "<c-r>",
-				cycle_open_buffers = "<tab>",
-				copy_relative_path_to_selected_files = "<c-y>",
-				send_to_quickfix_list = "<c-q>",
-			},
+		-- the floating window scaling factor. 1 means 100%, 0.9 means 90%, etc.
+		floating_window_scaling_factor = 0.95,
+		-- the type of border to use for the floating window. Can be many values,
+		-- including 'none', 'rounded', 'single', 'double', 'shadow', etc. For
+		-- more information, see :h nvim_open_win
+		yazi_floating_window_border = "rounded",
+		open_for_directories = true,
+		open_multiple_tabs = true,
+		keymaps = {
+			show_help = "<f1>",
+			open_file_in_vertical_split = "<c-v>",
+			open_file_in_horizontal_split = "<c-x>",
+			open_file_in_tab = "<c-t>",
+			grep_in_directory = "<c-f>",
+			replace_in_directory = "<c-r>",
+			cycle_open_buffers = "<tab>",
+			copy_relative_path_to_selected_files = "<c-y>",
+			send_to_quickfix_list = "<c-q>",
 		},
 	})
 end
