e9e9c61c61cf40872e052dc2c10ad871edb8ad36
tr1v3r <tr1v3r@outlook.com>
2026-07-17 02:25:21 +0800

    fix(treesitter): support Neovim 0.12 APIs

--- lua/plugins/configs/editor/treesitter.lua
+++ lua/plugins/configs/editor/treesitter.lua
@@ -1,74 +1,99 @@
-return vim.schedule_wrap(function()
-	vim.o.foldmethod = "expr"
-	vim.o.foldexpr = "nvim_treesitter#foldexpr()"
-
-	require("nvim-treesitter.configs").setup({
-		ensure_installed = require("r1v3r.settings").treesitter_deps,
-		highlight = {
-			enable = true,
-			disable = function(ft, bufnr)
-				if vim.tbl_contains({ "vim" }, ft) then
-					return true
-				end
-
-				local ok, is_large_file = pcall(vim.api.nvim_buf_get_var, bufnr, "bigfile_disable_treesitter")
-				return ok and is_large_file
-			end,
-			additional_vim_regex_highlighting = false,
-		},
-		incremental_selection = {
-			enable = true,
-			keymaps = {
-				init_selection = "<CR>",
-				node_incremental = "<CR>",
-				-- scope_incremental = "<CR>",
-				node_decremental = "<BS>",
-			},
-		},
-		textobjects = {
-			-- docs: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
-			select = {
-				enable = true,
-				keymaps = {
-					["af"] = "@function.outer",
-					["kf"] = "@function.inner",
-					["ac"] = "@class.outer",
-					["kc"] = "@class.inner",
-				},
-			},
-			move = {
-				enable = true,
-				set_jumps = true, -- whether to set jumps in the jumplist
-				goto_next_start = {
-					["]["] = "@block.outer",
-					["]f"] = "@function.outer",
-					["]m"] = "@class.outer",
-				},
-				goto_next_end = {
-					["]]"] = "@block.outer",
-					["]F"] = "@function.outer",
-					["]M"] = "@class.outer",
-				},
-				goto_previous_start = {
-					["[["] = "@block.outer",
-					["[f"] = "@function.outer",
-					["[m"] = "@class.outer",
-				},
-				goto_previous_end = {
-					["[]"] = "@block.outer",
-					["[F"] = "@function.outer",
-					["[M"] = "@class.outer",
-				},
-			},
-		},
-		indent = { enable = true },
-		matchup = { enable = true },
+local function setup_textobjects()
+	require("nvim-treesitter-textobjects").setup({
+		select = { lookahead = true },
+		move = { set_jumps = true },
 	})
-	require("nvim-treesitter.install").prefer_git = true
-	-- if use_ssh then
-	--	 local parsers = require("nvim-treesitter.parsers").get_parser_configs()
-	--	 for _, p in pairs(parsers) do
-	--		 p.install_info.url = p.install_info.url:gsub("https://github.com/", "git@github.com:")
-	--	 end
-	-- end
-end)
+
+	local select = require("nvim-treesitter-textobjects.select")
+	local move = require("nvim-treesitter-textobjects.move")
+
+	local selections = {
+		af = "@function.outer",
+		kf = "@function.inner",
+		ac = "@class.outer",
+		kc = "@class.inner",
+	}
+	for lhs, query in pairs(selections) do
+		local capture = query
+		vim.keymap.set({ "x", "o" }, lhs, function()
+			select.select_textobject(capture, "textobjects")
+		end, { desc = "Treesitter select " .. capture })
+	end
+
+	local motions = {
+		goto_next_start = {
+			["]["] = "@block.outer",
+			["]f"] = "@function.outer",
+			["]m"] = "@class.outer",
+		},
+		goto_next_end = {
+			["]]"] = "@block.outer",
+			["]F"] = "@function.outer",
+			["]M"] = "@class.outer",
+		},
+		goto_previous_start = {
+			["[["] = "@block.outer",
+			["[f"] = "@function.outer",
+			["[m"] = "@class.outer",
+		},
+		goto_previous_end = {
+			["[]"] = "@block.outer",
+			["[F"] = "@function.outer",
+			["[M"] = "@class.outer",
+		},
+	}
+	for method, mappings in pairs(motions) do
+		for lhs, query in pairs(mappings) do
+			local motion = method
+			local capture = query
+			vim.keymap.set({ "n", "x", "o" }, lhs, function()
+				move[motion](capture, "textobjects")
+			end, { desc = "Treesitter move to " .. capture })
+		end
+	end
+end
+
+return function()
+	local treesitter = require("nvim-treesitter")
+	local parsers = require("r1v3r.settings").treesitter_deps
+
+	treesitter.setup()
+	treesitter.install(parsers)
+
+	-- The parser is named "latex", while Neovim detects TeX buffers as "tex".
+	vim.treesitter.language.register("latex", { "tex", "plaintex" })
+
+	vim.api.nvim_create_autocmd("FileType", {
+		group = vim.api.nvim_create_augroup("r1v3r_treesitter", { clear = true }),
+		callback = function(args)
+			if args.match == "vim" then
+				return
+			end
+
+			local ok, is_large_file =
+				pcall(vim.api.nvim_buf_get_var, args.buf, "bigfile_disable_treesitter")
+			if ok and is_large_file then
+				return
+			end
+
+			local language = vim.treesitter.language.get_lang(args.match)
+			if not language or not vim.treesitter.language.add(language) then
+				return
+			end
+
+			vim.treesitter.start(args.buf, language)
+			vim.wo.foldmethod = "expr"
+			vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+			vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+		end,
+	})
+
+	vim.keymap.set({ "n", "x" }, "<CR>", function()
+		vim.treesitter.select("parent")
+	end, { desc = "Treesitter expand selection" })
+	vim.keymap.set("x", "<BS>", function()
+		vim.treesitter.select("child")
+	end, { desc = "Treesitter shrink selection" })
+
+	setup_textobjects()
+end
--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -119,17 +119,13 @@ editor["kylechui/nvim-surround"] = {
 ----------------------------------------------------------------------
 -- nvim-treesitter/playground Run :TSPlaygroundToggle show AST of current file
 editor["nvim-treesitter/nvim-treesitter"] = {
-	lazy = true,
-	build = function()
-		if #vim.api.nvim_list_uis() ~= 0 then
-			vim.api.nvim_command([[TSUpdate]])
-		end
-	end,
-	event = "BufReadPre",
+	branch = "main",
+	lazy = false,
+	build = ":TSUpdate",
 	config = require("editor.treesitter"),
 	dependencies = {
 		{ "andymass/vim-matchup" },
-		{ "nvim-treesitter/nvim-treesitter-textobjects" },
+		{ "nvim-treesitter/nvim-treesitter-textobjects", branch = "main" },
 		{
 			"abecodes/tabout.nvim",
 			keys = keymaps.tabout(),
